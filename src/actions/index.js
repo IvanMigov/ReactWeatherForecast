45572930eb0f3fc642244925956465f3f375ba01
ivan <ivan.migov@gmail.com>
2017-03-19 23:52:00 +0200

    sparklines

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = '7bb71013d42f298b1666697ad43a9571';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export  function fetchWeather(city) {
+  let url = `${ROOT_URL}&q=${city},us`;
+  let request = axios.get(url);
+
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+
+}
+
+
+
+
